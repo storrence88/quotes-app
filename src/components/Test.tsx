24395613e998e88ba0f5222ddf7fea0f69c53d7c
Steven Torrence <steve@cratebind.com>
2022-04-23 21:09:01 -0500

    Setup sass preprocessor and css modules

---
+++ src/components/Test.tsx
@@ -0,0 +1,9 @@
+import styles from '../stylesheets/Test.module.scss';
+
+const Test = () => {
+  return (
+    <div className={styles.highlight}>This is a test component</div>
+  );
+}
+
+export default Test;
